8bae6a3ee7c6a20830e1b9f7ae45beda96a93dee
timkellogg <tim.kellogg@gmail.com>
2016-07-09 22:59:39 -0700

    refactor and update url endpoints for server

--- hyfn8/server/router.js
+++ hyfn8/server/router.js
@@ -1,26 +1,26 @@
-const facebookService = require('./services/facebook');
 const request = require('request-promise');
+const FacebookService = require('./services/facebook');
 const Secrets = require('./secrets');
 const access_token = Secrets.access_token;
 const account_id = Secrets.account_id;
 const campaign_id = Secrets.campaign_id;
-
 const errorMessage = 'Something went wrong with Facebook credentials';
 const ROOT_URL = 'https://graph.facebook.com/v2.6/';
 
 module.exports = function(app) {
   app.get('/facebook', function(req, res) {
-
-    request(ROOT_URL + account_id + '/ads?access_token=' + access_token, function(err, response, body) {
-      const ads_response = JSON.parse(response.body);
+    request(ROOT_URL + account_id + '/ads?fields=name%2Cstatus%2Ccampaign_id&access_token=' + access_token, function(err, response, body) {
+      const ads_response = response.body;
 
     }).then(function(ads_response) {
-
-      request(ROOT_URL + campaign_id + '/insights?fields=impressions%2Creach%2Cfrequency%2Ccpm%2Cspend%2Cctr%2Ccost_per_inline_link_click%2Cactions%2Cunique_actions%2Ccost_per_action_type&access_token=' + access_token, function(err, response, body) {
+      request(ROOT_URL + campaign_id + '/insights?fields=impressions%2Creach%2Cfrequency%2Ccpm%2Cspend%2Cctr%2Ccost_per_inline_link_click%2Cactions%2Cunique_actions%2Ccost_per_action_type%2Ccampaign_id&access_token=' + access_token, function(err, response, body) {
 
         if (!err && response.statusCode == 200) {
           const metrics_response = JSON.parse(response.body);
-          const joined_response = facebookService.process({ ads: ads_response, metrics: metrics_response });
+          const joined_response = FacebookService.process({
+            ads: JSON.parse(ads_response),
+            metrics: metrics_response
+          });
 
           res.status(200).send({ data: joined_response });
         } else {
@@ -29,6 +29,6 @@ module.exports = function(app) {
       });
     }).catch(function(err) {
       res.status(500).send({ error: err });
-    })
+    });
   });
 }
--- hyfn8/server/services/facebook.js
+++ hyfn8/server/services/facebook.js
@@ -1,7 +1,63 @@
-module.exports = {
-  process: function(unprocessed_data) {
-    // TODO: clean data
+// 'use strict';
 
-    return unprocessed_data;
+// module.exports = {
+//   /**
+//     * Method that handles the merging of ads and campaign information
+//   */
+//   process: function(unprocessed_data) {
+//     let processed_data = [];
+
+//     unprocessed_data.metrics.data.forEach(function(metric) {
+//       let metric_object = {};
+
+//       for (let prop in metric) {
+//         if (prop === 'campaign_id') {
+//           unprocessed_data.ads.data.forEach(function(ad) {
+//             if (ad.campaign_id == metric[prop]) {
+//               metric_object['name'] = ad.name;
+//               return;
+//             }
+//           });
+//         }
+
+//         metric_object[prop] = metric[prop];
+//       }
+
+//       processed_data.push(metric_object);
+//     });
+
+//     return processed_data;
+//   }
+// }
+
+'use strict';
+
+module.exports = {
+  /**
+    * Method that handles the merging of ads and campaign information
+  */
+  process: function(unprocessed_data) {
+    let processed_data = [];
+
+    unprocessed_data.metrics.data.forEach(function(metric) {
+      let metric_object = {};
+
+      for (let prop in metric) {
+        if (prop === 'campaign_id') {
+          unprocessed_data.ads.data.forEach(function(ad) {
+            if (ad.campaign_id == metric[prop]) {
+              metric_object['name'] = ad.name;
+              return;
+            }
+          });
+        }
+
+        metric_object[prop] = metric[prop];
+      }
+
+      processed_data.push(metric_object);
+    });
+
+    return processed_data;
   }
 }
