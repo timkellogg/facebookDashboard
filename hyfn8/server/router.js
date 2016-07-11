043a35a9a3eb2c78fa1c590e3637406345d830a3
timkellogg <tim.kellogg@gmail.com>
2016-07-10 21:24:23 -0700

    change how env is loaded

--- hyfn8/server/router.js
+++ hyfn8/server/router.js
@@ -1,19 +1,28 @@
+'use strict';
+
 const request = require('request-promise');
 const FacebookService = require('./services/facebook');
-const Secrets = require('./secrets');
-const access_token = Secrets.access_token;
-const account_id = Secrets.account_id;
-const campaign_id = Secrets.campaign_id;
+const fs = require('fs');
+
+let Credentials;
+
+// handle environmental var loading better
+try {
+  Credentials = require('./secrets').Credentials;
+} catch (err) {
+  Credentials = process.env;
+}
+
 const errorMessage = 'Something went wrong with Facebook credentials';
 const ROOT_URL = 'https://graph.facebook.com/v2.6/';
 
 module.exports = function(app) {
   app.get('/facebook', function(req, res) {
-    request(ROOT_URL + account_id + '/ads?fields=name%2Cstatus%2Ccampaign_id&access_token=' + access_token, function(err, response, body) {
+    request(ROOT_URL + Credentials.account_id + '/ads?fields=name%2Cstatus%2Ccampaign_id&access_token=' + Credentials.access_token, function(err, response, body) {
       const ads_response = response.body;
 
     }).then(function(ads_response) {
-      request(ROOT_URL + campaign_id + '/insights?fields=campaign_id%2Cimpressions%2Creach%2Cfrequency%2Ccpm%2Cspend%2Cctr%2Ccost_per_inline_link_click%2Cactions%2Ccost_per_action_type%2Ccall_to_action_clicks&access_token=' + access_token, function(err, response, body) {
+      request(ROOT_URL + Credentials.campaign_id + '/insights?fields=campaign_id%2Cimpressions%2Creach%2Cfrequency%2Ccpm%2Cspend%2Cctr%2Ccost_per_inline_link_click%2Cactions%2Ccost_per_action_type%2Ccall_to_action_clicks&access_token=' + Credentials.access_token, function(err, response, body) {
 
         if (!err && response.statusCode == 200) {
           const metrics_response = JSON.parse(response.body);
